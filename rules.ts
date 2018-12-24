d4f2c3b5a7d56504fdd76a2f8cfad0c5acb73e87
Michael Jones <michael@zaptic.com>
2018-12-24 21:36:10 +0000

    Improve rule handling
    For calc, flex and various static string replacements & values.

--- rules.ts
+++ rules.ts
@@ -13,17 +13,34 @@ function suffix(name: string, count: number) {
     return `${name}${count}`
 }
 
+const replacements: { [key: string]: string } = {
+    nowrap: 'noWrap',
+    '0': 'zero',
+    '1': '(int 1)',
+    '-': 'minus',
+    '+': 'plus',
+}
+
 function translateValue(value: string) {
     if (/.*px/.test(value)) {
         return `(px ${value.replace('px', '')})`
     } else if (/.*em/.test(value)) {
         return `(em ${value.replace('em', '')})`
+    } else if (/.*vw/.test(value)) {
+        return `(vw ${value.replace('vw', '')})`
+    } else if (/.*vh/.test(value)) {
+        return `(vh ${value.replace('vh', '')})`
     } else if (/.*%/.test(value)) {
         return `(pct ${value.replace('%', '')})`
     } else if (/#.*/.test(value)) {
         return `(hex "${value.replace('#', '')}")`
     } else if (/\$.*/.test(value)) {
         return _.camelCase(value.replace('$', ''))
+    } else if (/[a-z]-[a-z]/.test(value)) {
+        // If the value is a string with a dash in it like 'space-between' then camelCase it
+        return _.camelCase(value)
+    } else if (replacements[value] !== undefined) {
+        return replacements[value]
     }
     return value
 }
@@ -32,17 +49,32 @@ function display(node: postcss.Declaration): ElmDecl {
     if (node.value === 'flex') {
         return { name: 'displayFlex', values: [] }
     }
-    return { name: 'display', values: [node.value] }
+    return { name: 'display', values: [_.camelCase(node.value)] }
+}
+
+export function flex(node: postcss.Declaration) {
+    const args = node.value.split(' ')
+
+    // Drop 'auto' entries from the end as Elm Css seems to consider them redundant
+    const values = _.dropRightWhile(args, arg => arg === 'auto').map(translateValue)
+    return { name: suffix(_.camelCase(node.prop), values.length), values }
 }
 
 export function standard(node: postcss.Declaration) {
-    const args = node.value.split(' ')
-    const values = args.map(translateValue)
-    return { name: suffix(_.camelCase(node.prop), values.length), values }
+    const match = node.value.match(/^calc\((.*)\)/)
+    if (match && match.length) {
+        const values = match[1].split(' ').map(translateValue)
+        return { name: node.prop, values: ['<|', 'calc', ...values] }
+    } else {
+        const args = node.value.split(' ')
+        const values = args.map(translateValue)
+        return { name: suffix(_.camelCase(node.prop), values.length), values }
+    }
 }
 
 type Lookup = { [key: string]: (node: postcss.Declaration) => ElmDecl }
 
 export const lookup: Lookup = {
     display,
+    flex,
 }
